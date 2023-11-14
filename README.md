925826efe98f68ebe4d75e73c6482df3f8848fae
T-Eek <twaneekhout@gmail.com>
2023-11-14 12:21:14 +0100

    Added the Le Point project to the git

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Le-Point
+[![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-24ddc0f5d75046c5622901739e7c5dd533143b0c8e959d652212380cedb1ea36.svg)](https://classroom.github.com/a/kY-M3P6n)
+# unity-project-the-dot
+template voor unity projecten
+
+[![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-24ddc0f5d75046c5622901739e7c5dd533143b0c8e959d652212380cedb1ea36.svg)](https://classroom.github.com/a/lGWcjHZE)
+
+## [Documentation](Documentation)
+In deze map sla je **alle** documentatie op die behoort bij het project.
+
+Maak zelf submappen voor de verschillende onderdelen.
+
+## [Source](Source)
+
+In deze map komt alle broncode
+
+## [Templates](Templates)
+
+In deze map staan de templates voor de documentatie.
+
+`Pas dit README.md bestand aan om de beoordelaars inzicht te geven in het project`
---
+++ Source/The Dot UnityPJ/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+//using UnityEngine.InputSystem;
+using TMPro;
+//using static UnityEditor.Timeline.TimelinePlaybackControls;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public TextMeshProUGUI countText;
+    private int count;
+    Camera cam;
+    private RayCastController rayCastController; // Reference to RayCastController
+    public LayerMask mask;
+    public ObjectSpawnerManager spawnerManager; // Reference to ObjectSpawnerManager
+
+    private float deactivationDelay = 0.2f; // Adjust the delay duration as needed
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rayCastController = GetComponent<RayCastController>(); // Get the reference to RayCastController
+        cam = Camera.main;
+        count = 0;
+
+        SetCountText();
+    }
+
+    IEnumerator DeactivateObjectAfterDelay(GameObject obj)
+    {
+        yield return new WaitForSeconds(deactivationDelay);
+        obj.SetActive(false);
+        SetCountText();
+        spawnerManager.OnObjectHit(); // Inform the ObjectSpawnerManager that an object was hit & spawns a new object
+        Debug.Log(obj.name + " is hit successfully");
+
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rayCastController.HandleRaycast(mask); // Use RayCastController for raycasting
+
+        // Your additional logic specific to PlayerController
+        // For example, handle additional behavior on mouse click
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100, mask))
+            {
+                if (hit.transform != null)
+                {
+                    Renderer renderer = hit.transform.GetComponent<Renderer>();
+
+                    if (renderer != null)
+                    {
+                        if (renderer.material.color != Color.red)
+                        {
+                            renderer.material.color = Color.red;
+                            if (hit.transform.gameObject.CompareTag("Clicked"))
+                            {
+                                StartCoroutine(DeactivateObjectAfterDelay(hit.transform.gameObject));
+                                count++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
