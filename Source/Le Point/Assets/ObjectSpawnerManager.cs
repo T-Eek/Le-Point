62f17cf2c081de7e20fc17d00fdeb8278f6b09b8
T-Eek <twaneekhout@gmail.com>
2023-11-22 14:19:59 +0100

    Create container for endless spawning Targets

--- Source/Le Point/Assets/ObjectSpawnerManager.cs
+++ Source/Le Point/Assets/ObjectSpawnerManager.cs
@@ -4,18 +4,23 @@ using UnityEngine;
 
 public class ObjectSpawnerManager : MonoBehaviour
 {
-    public GameObject TheDot; // The object prefab to spawn
+    public GameObject Object; // The object prefab to spawn
     public int xPos;
     public int yPos;
 
     public int dotCount;
     public int maxObjects = 5; // Maximum number of spawned objects allowed
 
+    private Transform objectsContainer; // Container to store spawned objects (set in the Unity Editor)
+
+
     // Start is called before the first frame update
     void Start()
     {
+        objectsContainer = new GameObject("LeTargetContainer").transform; // Create a new GameObject as the container
+
         // Start spawning objects
-        StartCoroutine(DotDrop());
+        StartCoroutine(TargetDrop());
     }
 
     private void Update()
@@ -23,7 +28,7 @@ public class ObjectSpawnerManager : MonoBehaviour
         //
     }
 
-    IEnumerator DotDrop()
+    IEnumerator TargetDrop()
     {
         while (true) // Infinite loop to keep spawning objects
         {
@@ -32,9 +37,13 @@ public class ObjectSpawnerManager : MonoBehaviour
                 xPos = Random.Range(-3, 3);
                 yPos = Random.Range(2, 6);
                 Vector3 spawnPosition = new Vector3(xPos, yPos, 11);
-                Instantiate(TheDot, spawnPosition, Quaternion.identity);
+                GameObject spawnedObject = Instantiate(Object, spawnPosition, Quaternion.identity);
+
+                // Set the container as the parent of the spawned object
+                spawnedObject.transform.parent = objectsContainer;
+
                 dotCount++;
-                Debug.Log("Spawned " + TheDot + dotCount + " at position: " + spawnPosition);
+                Debug.Log("Spawned: Le Target" + Object + dotCount + " at position: " + spawnPosition);
             }
             yield return new WaitForSeconds(0.5f); // Delay between object spawns
         }
